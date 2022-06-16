8c5c82e93689f1325c306a85aa8a025d7eb7b37f
schlagem <schlagem@informatik.uni-freiburg.de>
2022-06-16 20:01:24 +0200

    Added graph structure: TODO come up with smart way to sample (set indices and so on should be intuitive)

--- naslib/search_spaces/ofa/graph.py
+++ naslib/search_spaces/ofa/graph.py
@@ -17,10 +17,30 @@ class OnceForAllSearchSpace(Graph):
     """
     Implementation of the Once for All Search space.
     """
+
+    # it is queryable using the pretrained model
+    # TODO come back to this
+    QUERYABLE = True
+
     def __init__(self):
         super().__init__()
 
-        # TODO define graph here
+        """
+        5 units 7371 choices per layer 
+            -Each unit {2,3,4} layers with 9 (3x3) choices 
+                - Each layer kernel size {3,5,7}
+                - Each layer increases number of channels by {3,4,6}
+        """
+        # Graph definition
+        # we have 5 units
+        # maybe a smarter way to get the conditional layers
+        number_of_units = 5
+        self.add_nodes_from(range(1, number_of_units + 1))
+        self.add_edges_from([(i, i + 1) for i in range(1, number_of_units)])
+
+        # TODO add operations
+        raise NotImplementedError
+
 
     def query(self, metric: Metric, dataset: str, path: str) -> float:
         #TODO use pretrained OFA to query validation and test perfomance
